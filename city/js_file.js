5a626fc4976f1667ce21c30becee0b25995b21cc
ArevikT <areviktovmasyan7@gmail.com>
2019-02-02 13:21:44 +0400

    ...

--- city/js_file.js
+++ city/js_file.js
@@ -16,18 +16,7 @@ for( i = 0; i<8; i++ ){
     div1[i].style.width = "120px";
     div1[i].style.height = "120px";
 }
-// var x = document.getElementById("menus").style.display = "none"
-// function myFunction(){
-//     if(document.getElementById("menus").style.display == "none"){
-        
-//         document.getElementById("menus").style.display = "flex";
-//     }
-    
-//       else  document.getElementById("menus").style.display = "none"
-    
-// }
 
-// var x = document.getElementById("vertical").style.display = "none"
 function myFunction(){
     if(document.getElementById("vertical").style.display == "none"){
         
