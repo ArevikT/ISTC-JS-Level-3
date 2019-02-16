6e5f5969168fa4e280c9537021a9d7bb67c56971
ArevikT <areviktovmasyan7@gmail.com>
2019-02-16 15:47:23 +0400

    lesson oop

--- jQueryy_lesson/templateHomework/js_file.js
+++ jQueryy_lesson/templateHomework/js_file.js
@@ -11,5 +11,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1} ;   
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides,1500); 
+  setTimeout(showSlides,3000); 
 }
